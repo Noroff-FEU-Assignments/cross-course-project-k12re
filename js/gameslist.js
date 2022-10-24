6d3ed35335139bf4ff7c62c5ff8b1cd465f415a7
Ken Thore <kenthore@live.no>
2022-10-24 19:46:09 +0200

    call to API, for loop not working, made forEach to display html, working but need small fixes

--- js/gameslist.js
+++ js/gameslist.js
@@ -1,20 +1,50 @@
+const url = "https://gamehub.kenthore.no/wp-json/wc/store/products";
 const gamesList = document.querySelector(".game-cards");
 
-import games from "../js/gamesexport.js";
+//import games from "../js/gamesexport.js";
+
+async function fetchGames() {
+  try {
+    const response = await fetch(url);
+    const results = await response.json();
+    createHTML(results);
+    console.log(results);
+  }
+  catch(error) {
+    console.log(error);
+  }
+};
+
+fetchGames();
+
+function createHTML(games) {
+  games.forEach(results => {
+    gamesList.innerHTML += `<div class="game-card" id="${results.id}">
+    <a class="clickable" href="play.html">
+    <img class="game gameImg" src="${results.images[0].src}" alt="${results.name}"/>
+    <h2 class="gameName">${results.name}</h2>
+    <p class="gameDescription">${results.description}</p>
+    <a href="play.html"> Read more </a>
+    </a>
+  </div>`
+  }
+  );
+}
+
+//for (let i = 0; i < results.length; i++) {
+//  html += `<div class="game-card" id="${results.id}">
+//            <a class="clickable" href="play.html">
+//            <img class="game gameImg" src="${results.images}" alt="${results.name}"/>
+//            <h2 class="gameName">${results.name}</h2>
+//            <p class="gameDescription">${results.description}</p>
+//            <a href="play.html"> Read more </a>
+//            </a>
+//          </div>`
+
+//};
 
 let html = "";
 
-for (let i = 0; i < games.length; i++) {
-    html += `<div class="game-card" id="${games[i].id}">
-              <a class="clickable" href="play.html">
-              <img class="game gameImg" src="${games[i].img}" alt="${games[i].gameName}"/>
-              <h2 class="gameName">${games[i].gameName}</h2>
-              <p class="gameDescription">${games[i].description}</p>
-              <a href="play.html"> Read more </a>
-              </a>
-            </div>`
-
-};
-
 gamesList.innerHTML = html;
+
 
