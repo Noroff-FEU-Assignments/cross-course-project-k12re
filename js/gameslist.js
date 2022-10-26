5e9efda99b1bfb1354d43e2f9d7a23d7b5a66ba8
Ken Thore <kenthore@live.no>
2022-10-26 14:28:38 +0200

    added api call on gamepage (play) still few issues, if menu acces, page is "blank"

--- js/gameslist.js
+++ js/gameslist.js
@@ -1,8 +1,6 @@
 const url = "https://gamehub.kenthore.no/wp-json/wc/store/products";
 const gamesList = document.querySelector(".game-cards");
 
-//import games from "../js/gamesexport.js";
-
 async function fetchGames() {
   try {
     const response = await fetch(url);
@@ -20,7 +18,7 @@ fetchGames();
 function createHTML(games) {
   games.forEach(results => {
     gamesList.innerHTML += `<div class="game-card" id="${results.id}">
-    <a class="clickable" href="play.html">
+    <a class="clickable" href="play.html?id=${results.id}">
     <img class="game gameImg" src="${results.images[0].src}" alt="${results.name}"/>
     <h2 class="gameName">${results.name}</h2>
     <p class="gameDescription">${results.description}</p>
@@ -31,20 +29,6 @@ function createHTML(games) {
   );
 }
 
-//for (let i = 0; i < results.length; i++) {
-//  html += `<div class="game-card" id="${results.id}">
-//            <a class="clickable" href="play.html">
-//            <img class="game gameImg" src="${results.images}" alt="${results.name}"/>
-//            <h2 class="gameName">${results.name}</h2>
-//            <p class="gameDescription">${results.description}</p>
-//            <a href="play.html"> Read more </a>
-//            </a>
-//          </div>`
-
-//};
-
 let html = "";
 
 gamesList.innerHTML = html;
-
-
--- js/gamestore.js
+++ js/gamestore.js
@@ -1,9 +1,72 @@
-import games from "./gamesexport.js";
+const gameContainer = document.querySelector(".game-card");
+
+const queryString = document.location.search;
+let param = new URLSearchParams(queryString);
+let id = parseInt(param.get("id"));
+
+const newUrl = `https://gamehub.kenthore.no/wp-json/wc/store/products/` + id;
+
+console.log(id)
+
+//import games from "./gamesexport.js";
 
 const addToCart = document.querySelector(".cta-a");
 
 function purchase() {
     localStorage.setItem("games", JSON.stringify(games[0]));
 };
+
+async function fetchGame() {
+    try {
+      const response = await fetch(newUrl);
+      const results = await response.json();
+      
+      console.log(results);
+      //createHTML(results);
+      
+      gameContainer.innerHTML = "";
+
+      gameContainer.innerHTML += `<div class="game-card" id="${results.id}">
+                                <a class="clickable" href="play.html">
+                                <img class="game gameImg" src="${results.images[0].src}" alt="${results.name}"/>
+                                <h2 class="gameName">${results.name}</h2>
+                                <p class="gameDescription">${results.description}</p>
+                                <a href="checkout.html?id=${results.id}"> Add to cart </a>
+                                </a>
+                                </div>`;
+      
+    }
+    catch(error) {
+      console.log(error);
+    }
+
+    
+  };
+
+
+fetchGame();
+
+
+//function createHTML(gameContainer) {
+      
+//      };
+  
+//  let html = "";
+  
+//  gameContainer.innerHTML = html;
+
+
+//let html = "";
+
+//html.innerHTML += `<div class="game-card" id="${results.id}">
+ //       <a class="clickable" href="play.html">
+ //       <img class="game gameImg" src="${results.images}" alt="${results.name}"/>
+ //       <h2 class="gameName">${results.name}</h2>
+ //       <p class="gameDescription">${results.description}</p>
+ //       <a href="play.html"> Read more </a>
+ //       </a>
+ //       </div>`;
+
+
 
 addToCart.addEventListener("submit", purchase());
